05f15418aa3eb68b1649f42786e7728275275aaa
nileshkashani <nileshkashani45@gmail.com>
2026-07-22 21:06:07 +0530

    completed exercise 2.16-2.17

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,12 +2,22 @@ import { useEffect } from 'react'
 import { useState } from 'react'
 import axios from 'axios'
 
+const notification = (message) => {
+  return(
+  <div style={{border: '2px solid red' , margin: '10px' , padding: '10px'}}>
+    {message}
+  </div>
+  );
+}
+
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [number, setNumber] = useState(null)
   const [searchedName, setSearchedName] = useState('')
   const [isPhoneBookChanged, setIsPhoneBookChanged] = useState(false)
+  const [notificationMessage, setNotificationMessage] = useState('')
+  const [isNotificationActive, setIsNotificationActive] = useState(false)
 
   useEffect(() => {
     axios.get('http://localhost:3001/persons')
@@ -18,28 +28,72 @@ const App = () => {
   const submitName = async (event) => {
     event.preventDefault()
     if (persons.find(person => person.name === newName)) {
-      // console.log("if exected")
+      // console.log("if executed")
       if( confirm(`${newName} is already added to the phonebook do you want to replace the number?`) ){
         axios.put(`http://localhost:3001/persons/${persons.find(person => person.name === newName).id}`, {name: newName, number: number})
-        .then(res => {alert("number updated successfully !"); setIsPhoneBookChanged(!isPhoneBookChanged)})
+        .then(res => {
+          setIsPhoneBookChanged(!isPhoneBookChanged)
+          setNotificationMessage(`${newName} number is updated successfully`)
+          setIsNotificationActive(true)
+          setTimeout(() => {
+            setIsNotificationActive(false)
+          }, 5000)
+        })
+        .catch(err => {
+          setNotificationMessage(`${newName} failed to update number`)
+          setIsNotificationActive(true)
+          setTimeout(() => {
+            setIsNotificationActive(false)
+          }, 5000)
+        })
       }
     }
     else {
       // setPersons([...persons, { name: newName, number: number }])
       await axios.post('http://localhost:3001/persons', { name: newName, number: number })
-        .then((res) => { console.log(res); setIsPhoneBookChanged(!isPhoneBookChanged) })
-        .catch((err) => console.log(err))
+         .then(res => {
+          setIsPhoneBookChanged(!isPhoneBookChanged)
+          setNotificationMessage(`${newName} added to phonebook successfully!`)
+          setIsNotificationActive(true)
+          setTimeout(() => {
+            setIsNotificationActive(false)
+          }, 5000)
+        })
+        .catch((err) => {
+          setNotificationMessage(`failed to add ${newName} to phonebook`)
+          setIsNotificationActive(true)
+          setTimeout(() => {
+            setIsNotificationActive(false)
+          }, 5000)
+        })
     }
   }
 
   const handleDelete = (id) => {
     axios.delete(`http://localhost:3001/persons/${id}`)
-      .then(res => {alert(`${res.data.name} is deleted successfully`); setIsPhoneBookChanged(!isPhoneBookChanged)})
-      .catch(e => console.log(e))
+      .then(res => {
+        setNotificationMessage(`${res.data.name} is deleted successfully`)
+        setIsPhoneBookChanged(!isPhoneBookChanged)
+        setIsNotificationActive(true)
+        setTimeout(() => {
+          setIsNotificationActive(false)
+        }, 5000)
+      })
+      .catch(e => {
+        console.log("catch executed")
+        setNotificationMessage(`failed to delete because person with this record is already deleted`)
+        setIsNotificationActive(true)
+        setTimeout(() => {
+          setIsNotificationActive(false)
+        }, 5000)
+      })
   }
   return (
     <div>
       <h2>Phonebook</h2>
+      <div style={{height: isNotificationActive===true ? '10vh' : '0px' }}>
+        {isNotificationActive===true && notification(notificationMessage)}
+      </div>
       <form onSubmit={submitName}>
         <div>
           name: <input onChange={(e) => {
